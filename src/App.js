73f1628047421e529cff142830a80ac47fadaf43
Sunandar1717 <sunandar.developer@gmail.com>
2022-01-02 11:29:58 +0700

    membuat cv online

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import PortopolioList from './components/PortopolioList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-top">
+     <Navbar/>
+     <Hero/>
+     <PortopolioList/>
+     <About/>
+     <Contact/>
     </div>
   );
 }
